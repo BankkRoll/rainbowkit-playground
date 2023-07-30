3dd6edfe734d86206be3a4d62b45e5d5a604dd71
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-07-30 15:05:10 -0400

    fixy me fixalot

--- src/components/componentsList.ts
+++ src/components/componentsList.ts
@@ -1,8 +1,8 @@
 // components/componentsList.ts
 
 export const components = [
-    { label: 'Select a component', code: '' },
-    { label: 'ConnectButton', code: `return (
+  { label: 'Select a component', code: '' },
+  { label: 'Default ConnectButton', code: `return (
     <div
       style={{
         display: 'flex',
@@ -13,5 +13,68 @@ export const components = [
       <ConnectButton />
     </div>
   );` },
+  { label: 'ConnectButton with custom label', code: `return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'flex-end',
+        padding: 12,
+      }}
+    >
+      <ConnectButton label="Sign in" />
+    </div>
+  );` },
+  { label: 'ConnectButton showing only account avatar', code: `return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'flex-end',
+        padding: 12,
+      }}
+    >
+      <ConnectButton accountStatus="avatar" />
+    </div>
+  );` },
+  { label: 'ConnectButton showing only chain name', code: `return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'flex-end',
+        padding: 12,
+      }}
+    >
+      <ConnectButton chainStatus="name" />
+    </div>
+  );` },
+  { label: 'ConnectButton hiding balance', code: `return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'flex-end',
+        padding: 12,
+      }}
+    >
+      <ConnectButton showBalance={false} />
+    </div>
+  );` },
+  { label: 'ConnectButton responsive', code: `return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'flex-end',
+        padding: 12,
+      }}
+    >
+      <ConnectButton
+        accountStatus={{
+          smallScreen: 'avatar',
+          largeScreen: 'full',
+        }}
+        showBalance={{
+          smallScreen: false,
+          largeScreen: true,
+        }}
+      />
+    </div>
+  );` },
 ];
-  
